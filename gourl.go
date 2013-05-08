02e23eb0322b32f28fe746b20bd0a88503daaf40
Andrés Moreira <andres@andresmoreira.com>
2013-05-07 22:13:04 -0300

    fix tab/spaces

--- gourl.go
+++ gourl.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "net/http"
     "crypto/tls"
-	"strings"
+    "strings"
 )
 
 func main() {
-	var headers = flag.Bool("I", false, "Print headers information")
+    var headers = flag.Bool("I", false, "Print headers information")
     var insecure = flag.Bool("k", false, "Allow to perform insecure SSL connections")
-	var user_agent = flag.String("A", "gourl/1.0", "Set the User-Agent")
+    var user_agent = flag.String("A", "gourl/1.0", "Set the User-Agent")
 
-	flag.Parse()
-	if len(flag.Args()) <= 0 {
-		fmt.Printf("Usage: gourl [options] url\n")
-	} else {
-		url := flag.Arg(0)
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			fmt.Printf("Error creating request: %s\n", err)
-			return
-		}
-		req.Header.Add("User-Agent", *user_agent)
+    flag.Parse()
+    if len(flag.Args()) <= 0 {
+        fmt.Printf("Usage: gourl [options] url\n")
+    } else {
+        url := flag.Arg(0)
+        req, err := http.NewRequest("GET", url, nil)
+        if err != nil {
+            fmt.Printf("Error creating request: %s\n", err)
+            return
+        }
+        req.Header.Add("User-Agent", *user_agent)
 
         // Create TLS config
         tlsConfig := tls.Config{RootCAs: nil}
@@ -39,26 +39,26 @@ func main() {
             DisableCompression: true,
         }
         client := &http.Client{Transport: tr}
-		resp, err := client.Do(req)
+        resp, err := client.Do(req)
 
-		if err != nil {
-			fmt.Printf("An error ocurred: %s\n", err)
-			return
-		}
-		if *headers {
-			fmt.Printf("%s %s\n", resp.Proto, resp.Status)
-			for k, vals := range resp.Header {
-				fmt.Printf("%s: %s\n", k, strings.Join(vals, ";"))
-			}
-			fmt.Println()
-		} else {
-			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("An error ocurred: %s\n", err)
-				return
-			}
-			fmt.Printf("%s", body)
-		}
-	}
+        if err != nil {
+            fmt.Printf("An error ocurred: %s\n", err)
+            return
+        }
+        if *headers {
+            fmt.Printf("%s %s\n", resp.Proto, resp.Status)
+            for k, vals := range resp.Header {
+                fmt.Printf("%s: %s\n", k, strings.Join(vals, ";"))
+            }
+            fmt.Println()
+        } else {
+            defer resp.Body.Close()
+            body, err := ioutil.ReadAll(resp.Body)
+            if err != nil {
+                fmt.Printf("An error ocurred: %s\n", err)
+                return
+            }
+            fmt.Printf("%s", body)
+        }
+    }
 }
